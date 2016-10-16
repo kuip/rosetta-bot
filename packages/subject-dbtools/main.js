526203e8d87e25b0148232e56922fdbf3a1a573a
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-10-17 01:45:04 +0300

    remove collections

--- packages/subject-dbtools/main.js
+++ packages/subject-dbtools/main.js
@@ -5,3 +5,15 @@ import * as Baby from 'babyparse';
 import fs from 'fs';
 import { exec } from 'child_process';
 import { username, password } from './credentials.js';
+
+let removeCollections = (name) => {
+    let db = new arango.Database('http://' + username + ':' + password + '@127.0.0.1:8529');
+    db.useDatabase(name);
+
+    db.collections()
+    .then(collections => {
+        collections.forEach(c => {
+            c.drop();
+        });
+    });
+}
